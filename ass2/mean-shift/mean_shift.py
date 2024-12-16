67fd13065635e34db7325ced19919587ddf7fc1e
simicr <simradovan@gmail.com>
2024-12-16 18:27:24 +0100

    mean shift updated

--- ass2/mean-shift/mean_shift.py
+++ ass2/mean-shift/mean_shift.py
@@ -9,7 +9,7 @@ device = th.device('cuda') if th.cuda.is_available() else th.device('cpu')
 M = 128
 # The reference implementation works in chunks. Set this as high as possible
 # while fitting the intermediary calculations in memory.
-simul = M ** 2 // 16 #1024
+simul = M ** 2 // 4 #1024
 
 im = th.from_numpy(imageio.imread(f'./{M}.png') / 255.).to(device)
 
@@ -39,31 +39,25 @@ for zeta in [1,4]:
             # points. Note that for each point, you should compute the distance
             # to _all_ other points, not only the points in the current chunk.
             chunk = shifted[to_do[:simul]].clone()
-            
+
             # TODO: Mean shift iterations (15), writing back the result into shifted.
             # Compute distances between the current chunk and all points
-            
-            diff = chunk[:,None,:] - shifted[None,:,:] #(simul, M**2, 5)
-            #diff = chunk[:,None,:] - features[None,:,:]
-            distances_squared = th.sum(diff ** 2, dim=-1)  # Squared distances - calculates color and spatial distance?????
-                                                          # (simuli,M**2)
-            # Apply the kernel
-            in_bandwidth = distances_squared <= h_squared
-            weights = in_bandwidth.float() #(simuli,M**2)
-            card = weights.sum(dim=1) #(simuli)
 
-            # Compute the weighted mean (mean shift update)
-            weighted_sum = (weights[:,:,None] * shifted).sum(dim=1) #(simuli,5)
-            
-            updated_chunk = weighted_sum / card[:,None]
-            # Write back the result into shifted
-            shifted[to_do[:simul]] = updated_chunk #shifted[to_do[:simul]] = chunk.clone()
+            diff = chunk[:,None,:] - features[None,:,:]                      # (simul, M**2, 5)
+            distances_squared = th.sum(diff ** 2, dim=-1) / h_squared       # (simul, M**2)
+
+            indicator = distances_squared <= 1.0                            # (simul, M**2)
+            card = indicator.sum(dim=1)                                     # (simul)
+            neighbour_sum = (indicator[..., None] * features).sum(dim=1)     # (simul, 5)
+            updated_chunk = neighbour_sum / card[..., None]                 # (simul, 5)
+
+            shifted[to_do[:simul]] = updated_chunk
 
             # TODO: Termination criterion (17). cond should be True for samples
             # that need updating. Note that chunk contains the 'old' values.
-            movement = th.norm(updated_chunk - chunk, dim=-1) #norm w/ features
-            cond = movement >= threshold #True - cond not met
-            
+            movement = th.sum((updated_chunk - chunk) ** 2, dim=-1)     # (simul,)
+            cond = movement >= threshold                                # (simul,)
+
             # We only keep the points for which the stopping criterion is not met.
             # `cond` should be a boolean array of length `simul` that indicates
             # which points should be kept.
@@ -71,10 +65,9 @@ for zeta in [1,4]:
                 cond, cond.new_ones(to_do.shape[0] - cond.shape[0])
             ))]
             artist.set_data(shifted.view(M, M, 5).cpu()[:, :, :3])
-            #plt.pause(0.01)
+            plt.pause(0.01)
 
         # Reference images were saved using this code.
         imageio.imsave(
-    f'./results/{M}/zeta_{zeta:1.1f}_h_{h:.2f}.png',
-    (shifted.reshape(M, M, 5)[..., :3].clone().cpu() * 255).type(th.uint8).numpy()
-)
+            f'./results/{M}/zeta_{zeta:1.1f}_h_{h:.2f}.png',
+            (shifted.reshape(M, M, 5)[..., :3].clone().cpu() * 255).type(th.uint8).numpy())
